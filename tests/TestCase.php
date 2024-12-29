fafda366c72aca856d97766be7e47fc4145b0411
Sergey Kudashev <kudashevs@gmail.com>
2024-12-30 00:38:30 +0300

    Update TestCase add package aliases

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,34 @@
 
 namespace Kudashevs\LaravelParsedown\Tests;
 
+use Kudashevs\LaravelParsedown\Facades\ParsedownFacade;
 use Kudashevs\LaravelParsedown\Providers\ParsedownServiceProvider;
 
 class TestCase extends \Orchestra\Testbench\TestCase
 {
+    /**
+     * Get package providers.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
     protected function getPackageProviders($app)
     {
         return [
             ParsedownServiceProvider::class,
+        ];
+    }
+
+    /**
+     * Get package aliases.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
+    protected function getPackageAliases($app)
+    {
+        return [
+            'parsedown' => ParsedownFacade::class,
         ];
     }
 }
